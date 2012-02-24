084eac4527a0c093868f055f294a920ca41ffeb7
Debbie Chen <debbie.chen@communitech.ca>
2012-02-24 14:29:48 -0500

    Prescription label enlargens if 'can't read label' button is pressed

--- Other/iCY/javascript/test.js
+++ Other/iCY/javascript/test.js
@@ -1,10 +1,26 @@
 $(document).ready(function() {
 	document.getElementById('start').ontouchend=function(){ window.location = "index.html"};
-	document.getElementById('largerFontBtn').ontouchend=function(){ alert("enlarge font size")};
+	
+	document.getElementById('largerFontBtn').ontouchend=function(){
+		var labelSize = parseInt($('#instructionLabel').css('font-size'));
+		console.log ("original Label size: " + labelSize);
+		labelSize++; 
+		console.log ("modified Label size: " + labelSize);
+		$('#instructionLabel').css("font-size", labelSize + "pt");
+	};
+		
 	document.getElementById('done').ontouchend=function(){ 
-        			alert("still needs valudation. Continuing..."); 
-        			window.location = "patientfinished.html"};
+        			alert("still needs validation. Continuing..."); 
+        			window.location = "patientfinished.html"
+    };
+    
+    //generateInstruction();
 });
+
+var generateInstruction = function() {
+	var randomnumber=Math.floor(Math.random()*11);
+	document.getElementById('instructionLabel').innerHTML=instructionString;
+}
 
 head.js("https://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js","../javascript/ui.js","../javascript/touch.js", function (){
 	$("#touchme1, #touchme2, #touchme3, #touchme4, #touchme5, #touchme6").draggable({revert:true});
@@ -22,7 +38,7 @@ head.js("https://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js","../j
     	}
 	});
 });
-    
+
 function show(id){
 	var gen_box = document.getElementById(id);
     var gen_box_new = document.createElement('div');
