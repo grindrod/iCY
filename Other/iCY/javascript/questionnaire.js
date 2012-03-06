fa2388a7366b8eef0f7d312fb10df0668d6de477
Debbie Chen <debbie.chen@communitech.ca>
2012-03-06 12:04:01 -0500

    [FIXED #13][WIP #11] Slideout animation for follow-up questions. Currently font size is 16pt to accomodate space issues. Questions are side by side, so checkboxes are closer to label.

--- Other/iCY/javascript/questionnaire.js
+++ Other/iCY/javascript/questionnaire.js
@@ -1,3 +1,24 @@
-$("#glasses, #").live('click', function () {
-	alert("clicked");
+$(document).ready(function() {
+	$('#glassesnowContainer').hide();
+	$('#bifocalsnowContainer').hide();
+	$('#magnifiernowContainer').hide();
+    
+     $("#glasses, #bifocals, #magnifier").bind( "change", function(event, ui) {
+     	var state = $(this).is(':checked');
+  		var nowName = this.name + "now";
+  		var nowNameContainer = $('#' + nowName + "Container");
+  		
+  		if (state === true) { 
+  			nowNameContainer.slideDown();
+			$('#aidsUsed').trigger( 'updatelayout' );
+  		}
+  		else {
+  			nowNameContainer.slideUp();
+  			$('#' + nowName).prop("checked", false).checkboxradio('refresh');
+			$('#aidsUsed').trigger( 'updatelayout' );
+  		}
+
+		
+	});
+	
 });
