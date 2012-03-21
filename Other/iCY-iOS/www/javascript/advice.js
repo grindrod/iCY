2ab187bd3991a2b41de448bc4fd3a6324c9bacba
Debbie Chen <debbie.chen@communitech.ca>
2012-03-21 13:56:03 -0400

    Two separate implementations of Font Options in advice page based on device being used.

--- Other/iCY-iOS/www/javascript/advice.js
+++ Other/iCY-iOS/www/javascript/advice.js
@@ -12,36 +12,57 @@ var ADVICE = new Array (/*0*/ "Use the standard label (9-12pt font)",
 $(document).ready(function() {
 	$('#popupOptionsForPharmacy').hide();   
 
-});
+});*/
 
-function popOptions()
-{
-	if($('#popupOptionsForPharmacy').css('display') == 'none')
-	{
-		$('#popupOptionsForPharmacy').fadeIn('fast', function(){ $('#popupOptionsForPharmacy').show(); });
-	}
-	else
-	{
-		$('#popupOptionsForPharmacy').fadeOut('fast', function(){ $('#popupOptionsForPharmacy').hide(); });
-	}
+function popOptions(event) {
+    console.log('popOptions');
+    var popupSection = $('#popupOptionsForPharmacy');
+    popupSection.slideToggle();
+    
+    if ( popupSection.css('display') === 'none' ){
+        document.getElementById('advicePage').onclick=null;
+    }
+    else {
+        event.stopImmediatePropagation();
+        document.getElementById('advicePage').onclick=closeOptions;
+    }
 }
 
-function closeOptions()
-{
-	if($('#popupOptionsForPharmacy').css('display') != 'none')
-	{
-		$('#popupOptionsForPharmacy').fadeOut('fast', function(){ $('#popupOptionsForPharmacy').hide(); });
+function closeOptions() {
+    console.log('closeOptions!');
+    var popupSection = $('#popupOptionsForPharmacy');
+	if(popupSection.css('display') != 'none'){
+        console.log('apparently not displayed none?');
+        popupSection.slideToggle();
 	}
+    else {
+        console.log('display none!');
+    }
 }
-*/
+
+var deviceOptions = function() {
+    if ( navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPhone/i) ){
+        console.log('iPad!');
+        document.getElementById('fontOptionsBtn').onclick = popOptions;
+    }
+    else {
+        console.log('Playbook!');
+        $('#fontOptionsBtn').attr('data-rel', 'dialog');
+        $('#fontOptionsBtn').attr('href', '#pharmacy_options');
+        
+    }
+}
 
 $(document).ready(function() {
+    deviceOptions();
     document.ontouchmove = function(event){ event.preventDefault(); }
+    
+    $('#popupOptionsForPharmacy').hide();   
 	var results = analyseResults();
 	displayResults(results);
 });
 
-var analyseResults = function () {
+var analyseResults = function() {
 	var adviceToUse = JSON.parse( localStorage.getItem('adviceToUse') );
 	var userLevel = localStorage.getItem('userLevel');
 	console.log(userLevel);
@@ -87,5 +108,6 @@ var displayResults = function (adviceToUse){
 function fontOption(e)
 {
 	localStorage.setItem('instructionSizeLevel', JSON.stringify(e));
-	//var retrievedObject = localStorage.getItem('instructionSizeLevel');
+	var retrievedObject = localStorage.getItem('instructionSizeLevel');
+    console.log(retrievedObject);
 }
