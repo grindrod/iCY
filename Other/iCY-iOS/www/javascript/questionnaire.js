f1eed1731102de33dc8ec17200fb00d1efbfbb4e
Debbie Chen <debbie.chen@communitech.ca>
2012-04-16 13:03:51 -0400

    [#39] Fixed visual aids check.

--- Other/iCY-iOS/www/javascript/questionnaire.js
+++ Other/iCY-iOS/www/javascript/questionnaire.js
@@ -13,15 +13,6 @@ $(document).ready(function() {
   		if (state === true) { 
   			nowNameContainer.slideDown();
 			$('#aidsUsed').trigger( 'updatelayout' );
-			
-			/*var text;
-			if ( $(this)[0].name === 'magnifier') {
-				text = "If you have it with you, you may use it.";
-			} 
-			else { // glasses or bifocals
-				text = "If you have them with you, please put them on.";
-			}
-			showMessage(text); */
   		}
   		else {
   			nowNameContainer.slideUp();
@@ -34,10 +25,52 @@ $(document).ready(function() {
 	  	console.log("done clicked");
         var formData = $('form').serializeArray(); 
         console.log( formData );
-        
-        //aidCheck(formData);
                      
-        collectAdviceData(formData);
+        console.log("COLLECT ADVICE DATA");
+        var adviceToUse = JSON.parse( localStorage.getItem('adviceToUse') );
+        //console.log(adviceToUse);
+        var current;
+         
+        for (var i=0; i < formData.length; i++){
+            current = formData[i].name;
+            console.log("current[" +i + "]: " + current);
+         
+            // COLLECT ADVICE DATA
+            if (adviceToUse['discuss'] === false &&  formData[i].value === "on" && 
+                (formData[i].name === "largeprint" || formData[i].name === "prescription" || 
+                 formData[i].name === "nonprescription" || formData[i].name === "worn") ){
+                     adviceToUse['discuss'] = true;
+            }
+         
+            else if (formData[i].name === "glossy" && formData[i].value === "on") {
+                     adviceToUse['noTape'] = true;
+            }
+                     
+            
+            // VISUAL AID CHECK
+            var aidUsed;
+            if (formData[i].name === "glasses" || formData[i].name === "magnifier"){
+                console.log("formData length: " + formData.length);
+                increment = i+1 < formData.length? i+1 : i;
+                console.log("increment: " + increment);
+                console.log("increment OBJECT: " + formData[increment].name);
+                if (formData[increment].name === "glassesnow" || formData[increment].name === "magnifiernow"){
+                     console.log("User has visual aid now");
+                     onContinue();
+                }
+                else {
+                     console.log("has visual aid but does not have them now: " + current);
+                     aidUsed = $('#' + formData[i].name + 'Label').attr('value');
+                     //aidUsed = $('#' + current);
+                     console.log(aidUsed);
+                     showMessage(aidUsed);
+                }
+            }
+        }
+                     
+        
+         //console.log (  JSON.stringify(adviceToUse));
+         localStorage.setItem ('adviceToUse', JSON.stringify(adviceToUse) );
                      
   		/*if (window.location.href.search(/questionnaire1.html/) > 0){
   			window.location.href = 'questionnaire2.html';
@@ -64,7 +97,8 @@ var showMessage = function(usedAid) {
 	}
 }
 
-function onContinue(event){
+function onContinue(){
+    console.log("CONTINUE");
     if (window.location.href.search(/questionnaire1.html/) > 0){
         window.location.href = 'questionnaire2.html';
     }
@@ -76,60 +110,4 @@ function onContinue(event){
 function onCancel(event){
     $('.modalMessage').fadeOut('slow', function(){ $('.modalMessage').hide();});
 	$('#mainSection').fadeIn('slow', function(){ $('#mainSection').show();});
-}
-
-var aidCheck = function (formData){
-    for (var i in formData){
-        
-    }
-}
-
-var collectAdviceData = function(formData) {
-    console.log("COLLECT ADVICE DATA");
-    var adviceToUse = JSON.parse( localStorage.getItem('adviceToUse') );
-    //console.log(adviceToUse);
-    var current;
-    
-    for (var i in formData){
-        current = formData[i].name;
-        
-        // VISUAL AID CHECK
-        var aidUsed;
-        /*var increment = formData.length < i+1? i + 1 : i;
-        if ((formData[i].name === "glasses" && formData[increment].name != "glassesnow") ||
-            (formData[i].name === "magnifier" && formData[i+1].name != "magnifiernow")){
-            console.log("has visual aid but does not have them now");
-            aidUsed = $('#formData[i].name + "Label"').attr('value');
-            console.log(aidsUsed);
-            showMessage(aidsUsed);
-        }*/
-        
-        if (formData[i].name === "glasses" || formData[i].name === "magnifier"){
-            increment = formData.length < i+1? i+1 : i;
-            if (formData[increment].name === "glassesnow" || formData[increment].name === "magnifiernow"){
-                console.log("User has visual aid now");
-            }
-            else {
-                console.log("has visual aid but does not have them now: " + current);
-                aidUsed = $('#' + formData[i].name + 'Label').attr('value');
-                //aidUsed = $('#' + current);
-                console.log(aidUsed);
-                showMessage(aidUsed);
-            }
-            
-        }
-        
-        // COLLECT ADVICE DATA
-        if (adviceToUse['discuss'] === false &&  formData[i].value === "on" && 
-            (formData[i].name === "largeprint" || formData[i].name === "prescription" || 
-             formData[i].name === "nonprescription" || formData[i].name === "worn") ){
-  				adviceToUse['discuss'] = true;
-  			}
-        
-        else if (formData[i].name === "glossy" && formData[i].value === "on") {
-            adviceToUse['noTape'] = true;
-        }
-    }
-    console.log (  JSON.stringify(adviceToUse));
-    localStorage.setItem ('adviceToUse', JSON.stringify(adviceToUse) );
 }
