8eb0e1deb636cd6e7db34bc8874d722ac598f556
Debbie Chen <debbie.chen@communitech.ca>
2012-04-16 15:14:49 -0400

    Moved files around for clarity. Advice logic in questionnaire2 but NOT in questionnaire1.

---
+++ Other/iCY-iOS/www/javascript/questionnaire2.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+    document.ontouchmove = function(event){ event.preventDefault(); }
+    document.getElementById('done').onclick = onDone;
+});
+
+function onDone(e) {
+    var formData = $('form').serializeArray();
+    var adviceToUse = JSON.parse( localStorage.getItem('adviceToUse') );
+    var current;
+    
+    for (var i in formData){
+        current = formData[i];
+        console.log(current);
+        
+        if (current.name === "prescription"){
+            adviceToUse['difficultyReadLabels'] = "qYes";
+            adviceToUse['discussGlassLargePrint'] = "qYes";
+        }
+        else if (current.name === "nonprescription") {
+            adviceToUse['difficultyReadNonPrescription'] = true;
+            adviceToUse['discussMagnifying'] = true;
+        }
+        else if (current.name === "worn"){
+            adviceToUse['difficultyReadWorn'] = true;
+            adviceToUse['difficultyDiscuss'] = "qYes";
+        }
+        else if (current.name === "glossy"){
+            adviceToUse['difficultyReadGlossy'] = true;
+        }
+    }
+    
+    console.log (  adviceToUse );
+    localStorage.setItem ('adviceToUse', JSON.stringify(adviceToUse) );
+    window.location.href= 'instructions.html';
+}
