f58d3b9610a3557b082b3eeaf74ed027b60a0fcc
Debbie Chen <debbie.chen@communitech.ca>
2012-04-16 16:42:06 -0400

    [#38] Advice logic from the medications screen is now implemented.

--- Other/iCY-iOS/www/javascript/medications.js
+++ Other/iCY-iOS/www/javascript/medications.js
@@ -33,6 +33,28 @@ $(document).ready(function() {
 //				EVENT HANDLERS				//
 //////////////////////////////////////////////
 function onDone() {
+    var formData = $('form').serializeArray();
+    var adviceToUse = JSON.parse( localStorage.getItem('adviceToUse') );
+    var current;
+    
+    console.log(formData);
+    
+    for (var i in formData){
+        current = formData[i];
+        console.log("current: " + formData[i].name);
+        
+        if (current.name === "Corticosteriods" || current.name === "Anticholinergics"){
+            adviceToUse['counsel'] = true; 
+        }
+        else if (current.name === "Diabetes" || current.name === "Hypertension" ||
+                 current.name === "Glaucoma" || current.name === "Cataracts" ||
+                 current.name.search(/MacularDegeneration/i) >= 0 ){
+            adviceToUse['routineAssess'] = true;
+        }
+    }
+    
+    console.log(adviceToUse);
+    localStorage.setItem ('adviceToUse', JSON.stringify(adviceToUse) );
 	window.location.href='advice.html';
 }
 
@@ -44,8 +66,11 @@ var listenerForCheckbox = function() {
 
 
 function disableBtn(btn, state){
-	console.log('btn disabled: ' + state);
+	//console.log('btn disabled: ' + state);
 	var $btn = $('#'+btn);
+    
+    if ($btn.is(':disabled') === state){ return; }
+    
 	$btn.attr('disabled', state);
 	//$btn.addClass('btnDisabled');
 	
@@ -62,7 +87,9 @@ function disableBtn(btn, state){
 //				MODAL DIALOG				//
 //////////////////////////////////////////////
 function addCheckbox(name) {
-   	var html = '<input type="checkbox" id="'+name+'" value="'+name+'" class="custom" checked="true" /> <label for="'+name+'">'+name+'</label>';
+    var id = name.replace(/\s/g, '');
+    
+   	var html = '<input type="checkbox" id="'+id+'" name="'+id+'" class="custom" checked="true" /> <label for="'+id+'">'+name+'</label>';
    
 	$('#columnb').append(html);
    	$('#choices').trigger('create');
