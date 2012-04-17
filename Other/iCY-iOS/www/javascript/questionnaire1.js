caa40698f9480754ccb99122ed700e59fa00747d
Debbie Chen <debbie.chen@communitech.ca>
2012-04-17 15:39:53 -0400

    [#38] Questionnaire1 has the popups confirming that users have their visual aid.

--- Other/iCY-iOS/www/javascript/questionnaire1.js
+++ Other/iCY-iOS/www/javascript/questionnaire1.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
     document.ontouchmove = function(event){ event.preventDefault(); }
 	$('.modalMessage').hide();
-	$('#glassesnowContainer').hide();
-	$('#bifocalsnowContainer').hide();
-	$('#magnifiernowContainer').hide();
+    $('#modalMessage_cancel').hide();
+    $('#modalMessage_done').hide();
+    $('#modalMessage_ok').hide();
     
     //Centering questionnaire pages
       if($(document).height() >= 748)
@@ -12,19 +12,11 @@ $(document).ready(function() {
       }
                   
                   
-     $("#glasses, #bifocals, #magnifier").bind( "change", function(event, ui) {
+     $("#glasses, #magnifier, #helpWithMed").bind( "change", function(event, ui) {
      	var state = $(this).is(':checked');
-  		var nowName = this.name + "now";
-  		var nowNameContainer = $('#' + nowName + "Container");
   		
   		if (state === true) { 
-  			nowNameContainer.slideDown();
-			$('#aidsUsed').trigger( 'updatelayout' );
-  		}
-  		else {
-  			nowNameContainer.slideUp();
-  			$('#' + nowName).prop("checked", false).checkboxradio('refresh');
-			$('#aidsUsed').trigger( 'updatelayout' );
+            aidsCheck(this.name);
   		}
 	});
 	
@@ -43,68 +35,119 @@ $(document).ready(function() {
             console.log("current[" +i + "]: " + current);
          
             // COLLECT ADVICE DATA
-            if (adviceToUse['discuss'] === false &&  formData[i].value === "on" && 
-                (formData[i].name === "largeprint" || formData[i].name === "prescription" || 
-                 formData[i].name === "nonprescription" || formData[i].name === "worn") ){
-                     adviceToUse['discuss'] = true;
+            if (current.name === "largeprint"){
+                     adviceToUse['usesLargePrint'] = "qYes";
             }
          
             else if (formData[i].name === "glossy" && formData[i].value === "on") {
                      adviceToUse['noTape'] = true;
-            }
-                     
-            
-            // VISUAL AID CHECK
-            var aidUsed;
-            if (formData[i].name === "glasses" || formData[i].name === "magnifier"){
-                increment = i+1 < formData.length? i+1 : i;
-                if (formData[increment].name === "glassesnow" || formData[increment].name === "magnifiernow"){
-                     onContinue();
-                }
-                else {
-                     aidUsed = $('#' + formData[i].name + 'Label').attr('value');
-                     console.log(aidUsed);
-                     showMessage(aidUsed);
-                }
-            }
-            else {
-                onContinue();
             }
         }
                      
         
         //console.log (  JSON.stringify(adviceToUse));
         localStorage.setItem ('adviceToUse', JSON.stringify(adviceToUse) );
+        window.location.href = 'questionnaire2.html';
+        
   		return false;
 	});
 });
 
-var showMessage = function(usedAid) {
-    var text = "This is a measure of your ability to read prescription labels. If you require " + usedAid + " to read labels, we suggest that you wait to take the test when you have your visual aid.";
-    
-	if($('.modalMessage').css('display') == "none")
-	{
-		$('#mainSection').hide();
-		var docHeight = $(document).height();
-		$('.modalMessage').height(docHeight);
-		$('#modalMessage_text').html(text); 
-        document.getElementById('modalMessage_done').onclick=onContinue;
-        document.getElementById('modalMessage_cancel').onclick=onCancel;
-		$('.modalMessage').fadeIn('slow', function(){ $('.modalMessage').show();});
-	}
-}
 
-function onContinue(){
-    console.log("CONTINUE");
-    if (window.location.href.search(/questionnaire1.html/) > 0){
-        window.location.href = 'questionnaire2.html';
+
+var aidsCheck = function (aid){
+    var text; 
+    console.log("aid: " + aid);
+    if (aid === "glasses"){
+        $('#modalMessage_cancel').addClass('glassesCheck');
+        text = "Are you wearing them now?";
+        showMessage(text, "yesNo", closeModalDialog, appExplain);
+    }
+    else if (aid === "magnifier"){
+        $('#modalMessage_cancel').addClass('magnifierCheck');
+        text = "Are you using your magnifier now?";
+        showMessage(text, "yesNo", closeModalDialog, appExplain);
+    }
+    else if (aid === "helpWithMed"){
+        $('#modalMessage_cancel').addClass('helpCheck');
+        text = "Are they here with you now?";
+        showMessage(text, "yesNo", closeModalDialog, appExplain);
     }
     else {
-        window.location.href= 'instructions.html';
+        console.log("wut? 0.o");
     }
 }
 
-function onCancel(event){
+
+
+
+//////////////////////////////////
+//          MODAL DIALOG        //
+//////////////////////////////////
+
+var showMessage = function(text, messageClass, yesHandler, noHandler) {
+    console.log("SHOW MESSAGE");
+    
+    $('#mainSection').hide();
+	var docHeight = $(document).height();
+	$('.modalMessage').height(docHeight);
+    $('#modalMessage_text').hide(); 
+    
+    var yesText;
+    var noText;
+    if (messageClass == "yesNo"){
+        yesText = "YES";
+        noText = "NO";
+        document.getElementById('modalMessage_done').innerHTML=yesText;
+        document.getElementById('modalMessage_cancel').innerHTML=noText;
+        document.getElementById('modalMessage_done').onclick=yesHandler;
+        document.getElementById('modalMessage_cancel').onclick=noHandler;
+        $('#modalMessage_cancel').show();
+        $('#modalMessage_done').show();
+    }
+    else if (messageClass == "okOnly"){
+        yesText = "OK";
+        document.getElementById('modalMessage_ok').innerHTML=yesText;
+        document.getElementById('modalMessage_ok').onclick=yesHandler;  //change to redirect to index
+        $('#modalMessage_cancel').hide();
+        $('#modalMessage_done').hide();
+        $('#modalMessage_ok').show();
+    }
+    
+    $('#modalMessage_text').html(text);
+	$('#modalMessage_text').fadeIn('slow', function(){ $('#modalMessage_text').show();});
+    $('.modalMessage').fadeIn('slow', function(){ $('.modalMessage').show();});
+}
+
+function appExplain(event) {
+    var text;
+    var okHandler; 
+    
+    if (this.className.search(/glassesCheck/) >= 0){
+        text = "This app is checking how you read medication labels. Please wait until you have your glasses or contacts to try the app.";
+        okHandler = noVisualAid;
+    }
+    else if (this.className.search(/magnifierCheck/) >= 0){
+        text = "This app is checking how you read medication labels. Please ask the pharmacist for a magnifier to complete the test if needed.";
+        okHandler = closeModalDialog; 
+    }
+    else if (this.className.search(/helpCheck/) >= 0){
+        text = "This app is checking how you read medication labels. Please repeat the test when your support person is with you.";
+        okHandler = noVisualAid;
+    }
+    
+    showMessage(text, "okOnly", okHandler, null);
+}
+
+function closeModalDialog(){
     $('.modalMessage').fadeOut('slow', function(){ $('.modalMessage').hide();});
 	$('#mainSection').fadeIn('slow', function(){ $('#mainSection').show();});
+    $('#modalMessage_cancel').hide();
+    $('#modalMessage_done').hide();
+    $('#modalMessage_ok').hide();
+}
+
+function noVisualAid(){
+    console.log("no visual aid!");
+    window.location.href="index.html";
 }
