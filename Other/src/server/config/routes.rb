8ad1b030e3b4b8f2e53cc56d9a8654b9b09626fd
Daniele Hohol <danielehohol@Daniele-Hohols-MacBook-Pro.local>
2012-05-15 11:46:35 -0400

    simplified MVC to just use data

--- Other/src/server/config/routes.rb
+++ Other/src/server/config/routes.rb
@@ -1,7 +1,7 @@
 Server::Application.routes.draw do
   resources :data
 
-  get "home/index"
+  get "data/index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
